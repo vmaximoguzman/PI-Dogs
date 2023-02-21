c0e0d8f0491995d9b981f6500b862b72726f58bf
Maximo Guzman <vmaximoguzman@gmail.com>
2023-02-21 13:43:28 -0300

    Navbar y sus Estilos

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+//
+import style from "./Navbar.module.css";
+import SearchBar from "../SearchBar/SearchBar";
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
-    <>
-      <h4>Soy el componente Navbar</h4>
-      <NavLink to="/about">About |</NavLink>
-      <NavLink to="/contact">| Contact</NavLink>
-    </>
+    <div className={style.navDiv}>
+      <div className={style.divTitle}>
+        <NavLink to="/home" className={style.titleLink}>
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/007/559/104/original/dog-dish-menu-logo-design-template-modern-animal-icon-label-for-store-veterinary-food-free-vector.jpg"
+            alt="Logo Dog"
+            className={style.logoDog}
+          />
+          <h3 className={style.title}>Mundo Perruno</h3>
+        </NavLink>
+      </div>
+
+      <div>
+        <SearchBar onSearch={props.onSearch} />
+      </div>
+    </div>
   );
 };
 
