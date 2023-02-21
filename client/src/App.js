32933573730693c56feb79fde12cc8d84f9ed0c6
Maximo Guzman <vmaximoguzman@gmail.com>
2023-02-21 13:44:36 -0300

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import { Route } from "react-router-dom";
+import axios from "axios";
 //Rutas
 import Landing from "./components/Landing/Landing";
 import Home from "./components/Home/Home";
 
 function App() {
+  const onSearch = (dog) => {
+    axios
+      .get(`http://localhost:3001/dogs?name=${dog}`)
+      .then((res) => console.log(res.data));
+  };
+
   return (
     <div>
       <Route exact path="/">
@@ -11,7 +18,7 @@ function App() {
       </Route>
 
       <Route exact path="/home">
-        <Home />
+        <Home onSearch={onSearch} />
       </Route>
     </div>
   );
